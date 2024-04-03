435b8c24eb69f10ef07a20c7b7c2f24185e73527
vanberkom <vanberkom.luke@gmail.com>
2024-04-03 18:18:11 -0500

    Added the author challenge and session tracking.

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre
+from django.views import generic
 
 def index(request):
     """View function for home page of site."""
@@ -11,8 +12,13 @@ def index(request):
     # Available books (status = 'a')
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
 
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
     # Books with the in the title
     num_the_books = Book.objects.filter(title__icontains='the').count()
+
 
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
@@ -24,7 +30,23 @@ def index(request):
         'num_authors': num_authors,
         'num_genres': num_genres,
         'num_the_books': num_the_books,
+        'num_visits': num_visits,
     }
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 5
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 5
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+    
