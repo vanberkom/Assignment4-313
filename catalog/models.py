61dcf8cb9208abafc450ddf6d21d137a29df6797
vanberkom <vanberkom.luke@gmail.com>
2024-04-03 22:48:51 -0500

    Completed part 9 and did the challenge

--- catalog/models.py
+++ catalog/models.py
@@ -98,7 +98,7 @@ class BookInstance(models.Model):
     book = models.ForeignKey('Book', on_delete=models.RESTRICT, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
-
+    
     borrower = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True)
 
     language = models.ManyToManyField(Language, help_text="Select a language for this book")
